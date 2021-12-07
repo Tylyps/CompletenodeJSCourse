7a0ffc2fbd56c902a7aaa8e4c26d92fa2d46a452
Tylyps <jakubb.chmielewskii@gmail.com>
2021-12-08 00:26:07 +0100

    End of episode 11 - importing npm modules

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,6 +1,9 @@
+const validator = require("validator");
 const getNotes = require("./notes");
 
 console.log(getNotes());
+
+console.log(validator.isEmail("test@test.test"))
 
 // const add = require("./utils.js");
 // console.log(add(1,2));
