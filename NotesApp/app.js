0babe26a7edd0f6c5525c9365854399e03c13a79
Tylyps <jakubb.chmielewskii@gmail.com>
2021-12-08 00:41:36 +0100

    End of episode 12 - Printing in color

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,9 +1,12 @@
-const validator = require("validator");
+// const validator = require("validator");
 const getNotes = require("./notes");
+const chalk = require("chalk");
 
 console.log(getNotes());
 
-console.log(validator.isEmail("test@test.test"))
+console.log(chalk.bold.green("Success!"))
+
+// console.log(validator.isEmail("test@test.test"))
 
 // const add = require("./utils.js");
 // console.log(add(1,2));
