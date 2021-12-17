f9948afe3ede550ab70ed5bff5826b6662cc1fce
Tylyps <jakubb.chmielewskii@gmail.com>
2021-12-17 02:26:38 +0100

    End of episode 21 - arrow functions

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,30 @@
+// const square = function (x) {
+// 	return x * x;
+// };
+
+// const square = (x) => {
+// 	return x * x;
+// };
+
+// const square = (x) => x * x;
+// console.log(suqre(3))
+
+const event = {
+	name: "BIrhtday party",
+	guestList: ["1", "2", "3"],
+	// printGuestList: function() { good
+	// 	console.log("Guest list for " + this.name);
+	// }
+	// printGuestList: () => { bad
+	// 	console.log("Guest list for " + this.name);
+	// }
+	printGuestList() { // best
+		console.log("Guest list for " + this.name);
+
+		this.guestList.forEach(function (guest) {
+			console.log(guest + " is attending" + this.name);
+		});
+	}
+}
+
+event.printGuestList();
