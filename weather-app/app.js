1d1c49efa95bf105159ee0588477364038bc181b
Tylyps <jakubb.chmielewskii@gmail.com>
2022-02-02 06:33:49 +0100

    End of section 32 - customizing http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = "https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6"
 
-request({ url }, (error, response) => {
-	const data = JSON.parse(response.body)
-	console.log(data)
+request({ url, json: true }, (error, response) => {
+	console.log(response)
 })
