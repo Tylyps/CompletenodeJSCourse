dc338ec1901a8eb3c64baae35f886d8ce1a7a16d
Tylyps <jakubb.chmielewskii@gmail.com>
2022-02-05 21:31:17 +0100

    End of section 33 - http reauest for mapbox

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,17 @@
 const request = require('request')
 
-const url = "https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6"
+// const url = "https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6"
 
-request({ url, json: true }, (error, response) => {
-	console.log(response)
-})
+// request({ url, json: true }, (error, response) => {
+// 	console.log(response)
+// })
+
+const mapUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=1&access_token=pk.eyJ1IjoibWNza25lcnVzIiwiYSI6ImNrejU0cGx4aTBhcmoydXF3cWp3c2w2MTAifQ.ktlFLIA6AJb5kYaouasWwg"
+
+request({ url: mapUrl, json: true }, (error, response) => {
+	console.log(response.body.features[0].center);
+	const latitude = response.body.features[0].center[1];
+	const longtude = response.body.features[0].center[0];
+
+	console.log("latitude " + latitude + ", longtude" + longtude);
+});
