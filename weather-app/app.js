e7d4651a32c6001a26d81ed0b2f2e765b61c80c5
Tylyps <jakubb.chmielewskii@gmail.com>
2022-02-05 22:21:39 +0100

    end of section 34 - hendling errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,26 @@
 const request = require('request')
 
-// const url = "https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6"
-
-// request({ url, json: true }, (error, response) => {
-// 	console.log(response)
-// })
-
-const mapUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=1&access_token=pk.eyJ1IjoibWNza25lcnVzIiwiYSI6ImNrejU0cGx4aTBhcmoydXF3cWp3c2w2MTAifQ.ktlFLIA6AJb5kYaouasWwg"
+request({ url: weatherUrl, json: true }, (error, response) => {
+	if (error) {
+		console.log("Unable to connect to weather service!");
+	} else if (response.body.error) {
+		console.log("Unable to find location!");
+	} else {
+		const current = response.body.current
+		console.log(current.weather_descriptions + ". It is currently " + current.temperature)
+	}
+})
 
 request({ url: mapUrl, json: true }, (error, response) => {
-	console.log(response.body.features[0].center);
-	const latitude = response.body.features[0].center[1];
-	const longtude = response.body.features[0].center[0];
+	if (error) {
+		console.log("Unable to connect to location services!");
+	} else if (response.body.features == undefined || response.body.features.length <= 0) {
+		console.log("Unable to find location! Try different search phrase");
+	} else {
+		console.log(response.body.features[0].center);
+		const latitude = response.body.features[0].center[1];
+		const longtude = response.body.features[0].center[0];
 
-	console.log("latitude " + latitude + ", longtude" + longtude);
+		console.log("latitude " + latitude + ", longtude" + longtude);
+	}
 });
