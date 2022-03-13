5d97c4c9fd676cae6e9b7b1c6728a122a77d8404
Tylyps <jakubb.chmielewskii@gmail.com>
2022-03-13 15:22:48 +0100

    End of section 39 - object shorthand and destructuring

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,39 @@
+// Object property shorthand
+const name = "Test";
+const userAge = 25;
+
+// const user = {
+// 	name: name,
+// 	age: userAge,
+// 	location: "Warsaw"
+// }
+
+const user = {
+	name,
+	age: userAge,
+	location: "Warsaw"
+};
+
+//Object destructuring
+
+const product = {
+	label: "Red notebook",
+	price: 3,
+	stock: 201,
+	salePrice: undefined
+};
+
+// const label = product.label;
+// const stock = product.stock;
+
+const { label: productLabel, stock, rating = 5 } = product;
+
+console.log(productLabel)
+console.log(stock)
+console.log(rating)
+
+const transaction = (type, { label, stock }) => {
+	console.log(label)
+}
+
+transaction("order", product)
