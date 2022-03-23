d34a9a25d957d0e3bc318a5cd957183296f3657e
Tylyps <jakubb.chmielewskii@gmail.com>
2022-03-23 19:14:00 +0100

    End of section 47 - dynamic pages with temp

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,10 +5,32 @@ console.log(__dirname)
 console.log(path.join(__dirname, "../public"))
 
 const app = express();
-const publicPath = path.join(__dirname, "../public")
+const publicPath = path.join(__dirname, "../public");
 let test = 0;
 
+app.set("view engine", "hbs");
+
 app.use(express.static(publicPath))
+
+app.get("", (req, res) => {
+	res.render("index", {
+		title: "Weather App",
+		name: "Test Testing"
+	})
+})
+
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "About App",
+	})
+})
+
+app.get("/help", (req, res) => {
+	res.render("help", {
+		title: "Help App",
+		message: "Test message"
+	})
+})
 
 app.get("/weather", (req, res) => {
 	res.send({
