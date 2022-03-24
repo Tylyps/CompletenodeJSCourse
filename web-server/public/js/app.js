f35e435566ac6a3b569043cbf04a0f6405d5a294
Tylyps <jakubb.chmielewskii@gmail.com>
2022-03-24 20:59:22 +0100

    End of lesson 59 - writing up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,25 @@
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1")
+const messageTwo = document.querySelector("#message-2")
 
 weatherForm.addEventListener("submit", (e) => {
 	e.preventDefault()
 	console.log(search.value)
 
+	messageOne.textContent = "Loading..."
+	messageTwo.textContent = ""
 	fetch(`/weather?address=${search.value}`)
 	.then((res) => {
 		return res.json()
 	})
 	.then((data) => {
 		if (data.error) {
+			messageOne.textContent = data.error
 			console.log(data.error)
 		} else {
+			messageOne.textContent = data.location
+			messageTwo.textContent = `Weather is ${data.forecast.weatherDescriptions}. Temperature is ${data.forecast.temperature}`
 			console.log(data.location)
 			console.log(data.forecast)
 		}
