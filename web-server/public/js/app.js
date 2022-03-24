98e16ce2581515670ab2faec0dfb4d0fe04ab60f
Tylyps <jakubb.chmielewskii@gmail.com>
2022-03-24 20:37:39 +0100

    End of lesson 58 - creating a search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,4 +1,11 @@
-fetch("/weather?address=warszawa")
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (e) => {
+	e.preventDefault()
+	console.log(search.value)
+
+	fetch(`/weather?address=${search.value}`)
 	.then((res) => {
 		return res.json()
 	})
@@ -10,3 +17,4 @@ fetch("/weather?address=warszawa")
 			console.log(data.forecast)
 		}
 	})
+})
