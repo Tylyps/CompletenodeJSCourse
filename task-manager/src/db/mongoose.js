5984c60160d97184d1983cfdda52978847664fc5
Tylyps <jakubb.chmielewskii@gmail.com>
2022-04-05 23:36:49 +0200

    End of lesson 85 - Data validation and sanitization part 1

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,3 +1,4 @@
+const validator = require("validator")
 const mongoose = require("mongoose");
 
 const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
@@ -6,17 +7,58 @@ mongoose.connect(connectionURL);
 
 const User = mongoose.model("User", {
 	name: {
-		type: String
+		type: String,
+		require: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		require: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error("Email is invalid")
+			}
+		}
 	},
 	age: {
-		type: Number
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error("Age must be a positive number")
+			}
+		}
 	}
 });
 
-const me = new User({ name: "Test", age: "dsa" });
+const me = new User({ name: "Test2", email: "dsa" });
 
-me.save().then(() => {
-	console.log(me)
-}).catch((error) => {
-	console.log("Error!", error)
-})
+// me.save().then(() => {
+// 	console.log(me)
+// }).catch((error) => {
+// 	console.log("Error!", error)
+// })
+
+const Task = mongoose.model("Task", {
+	description: {
+		type: String,
+		trim: true
+	},
+	completed: {
+		type: Boolean,
+		default: false
+	}
+});
+
+// const newTask = new Task({ description: "New task 1", completed: false });
+
+// newTask.save()
+// 	.then((v) => {
+// 		console.log(v);
+// 		console.log(newTask);
+// 	})
+// 	.catch((error) => {
+// 		console.log(error);
+// 	})
