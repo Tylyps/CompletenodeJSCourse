3c852ea3c55e3b78bc1646f905b8d87995a8ef56
Tylyps <jakubb.chmielewskii@gmail.com>
2022-04-05 23:01:49 +0200

    End of lessos 83 - Setting up mongoose

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
+
+mongoose.connect(connectionURL);
+
+const User = mongoose.model("User", {
+	name: {
+		type: String
+	},
+	age: {
+		type: Number
+	}
+});
+
+const me = new User({ name: "Test", age: "dsa" });
+
+me.save().then(() => {
+	console.log(me)
+}).catch((error) => {
+	console.log("Error!", error)
+})
