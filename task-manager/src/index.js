8c2d5cd94a09a429ecc2b8fe5f3dfae7fa11f64f
Tylyps <jakubb.chmielewskii@gmail.com>
2022-04-09 01:03:25 +0200

    End of lesson 106 - JSON web tokens - learn about jst sign and verify

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -16,3 +16,15 @@ app.use(taskRouter);
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
+
+const jwt = require("jsonwebtoken")
+
+const myFunction = async () => {
+	const token = jwt.sign({ _id: "6250b97129ce90a91228a1ae" }, "thisismynewcourse", { expiresIn: "1 second" });
+	console.log(token);
+
+	const data = jwt.verify(token, "thisismynewcourse");
+	console.log(data)
+};
+
+myFunction()
