5c4b295c89b2788827fbd85db7ef4389fd4ed437
Tylyps <jakubb.chmielewskii@gmail.com>
2022-04-10 17:14:32 +0200

    End of lesson 108 - Express Middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,19 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+
+// 	if (req.method === "GET") {
+// 		res.status(403).send("Get request are disabled")
+// 	} else {
+// 		next();
+// 	}
+// })
+
+app.use((req, res, next) => {
+	res.status(503).send("Work in progress, try later.")
+})
+
 //Middleware
 app.use(express.json())
 
