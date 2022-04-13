886d941b3633c20f217e84490f7f3fe944eef183
Tylyps <jakubb.chmielewskii@gmail.com>
2022-04-13 20:52:10 +0200

    End of lesson 124 - validating file uploads

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,14 +7,23 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-// const multer = require("multer");
-// const upload = multer({
-// 	dest: "images"
-// })
+const multer = require("multer");
+const upload = multer({
+	dest: "images",
+	limits: {
+		fileSize: 1 * 1000 * 1000
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(doc|docx)$/)) {
+			return cb(new Error("Please upload a Word document"));
+		}
+		cb(undefined, true);
+	}
+})
 
-// app.post("/upload", upload.single("upload"), (req, res) => {
-// 	res.send()
-// })
+app.post("/upload", upload.single("upload"), (req, res) => {
+	res.send()
+})
 
 
 //Middleware
