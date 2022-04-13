6fd0bd4b52f241698d6694ac29ef81a6de095a68
Tylyps <jakubb.chmielewskii@gmail.com>
2022-04-14 00:46:42 +0200

    End of lesson 129 - auto cropping and image formatting

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const sharp = require("sharp");
 
 const User = require("../models/user");
 const { auth } = require("../middleware/auth");
@@ -52,7 +53,13 @@ const upload = multer({
 })
 
 router.post("/users/me/avatar", auth, upload.single("avatar"), async (req, res) => {
-	req.user.avatar = req.file.buffer
+	// const buffer = req.file.buffer
+	const buffer = await sharp(req.file.buffer)
+		.resize({ width: 250, height: 250 })
+		.png()
+		.toBuffer()
+
+	req.user.avatar = buffer
 	try {
 		await req.user.save()
 		res.send()
@@ -83,7 +90,7 @@ router.get("/users/:id/avatar", async (req, res) => {
 			throw new Error("User not found")
 		}
 
-		res.set("Content-Type", "image/jpg")
+		res.setHeader("Content-Type", "image/png")
 
 		res.send(user.avatar)
 	} catch(e) {
