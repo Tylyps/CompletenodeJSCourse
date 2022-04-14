bf6388199da4720486b8c5d1125db4b094b8a886
Tylyps <jakubb.chmielewskii@gmail.com>
2022-04-14 22:17:01 +0200

    End of lesson 131-133 - exploring emails with sendgrid and enviroment vars

---
+++ task-manager/src/emails/account.js
@@ -0,0 +1,28 @@
+const sgMail = require("@sendgrid/mail");
+
+const sendgridAPIkey = process.env.SEND_GRID_KEY;
+
+sgMail.setApiKey(sendgridAPIKey);
+
+const sendWelcomeEmail = (email, name) => {
+	sgMail.send({
+		to: email,
+		from: "test@test.test",
+		subject: "Thanks for joining in!",
+		text: `Welcome to the app, ${name}. LEt me know how you get along with the app.`
+	})
+}
+
+const sendCancelationEmail = (email, name) => {
+	sgMail.send({
+		to: email,
+		from: "test@test.test",
+		subject: "Your accout was deleted",
+		text: `${name}, your account was deleted. Could you let us know what can we improve in our app to be better.`
+	})
+}
+
+module.exports = {
+	sendWelcomeEmail,
+	sendCancelationEmail
+}
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -4,6 +4,7 @@ const sharp = require("sharp");
 
 const User = require("../models/user");
 const { auth } = require("../middleware/auth");
+// const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account");
 
 const router = new express.Router();
 
@@ -12,6 +13,7 @@ router.post("/users", async (req, res) => {
 
 	try {
 		await user.save();
+		// sendWelcomeEmail(user.email, user.name);
 		const token = await user.generateAuthToken();
 
 		res.status(201).send({
@@ -158,7 +160,8 @@ router.delete("/users/me", auth, async (req, res) => {
 			return res.status(404).send()
 		}
 
-		req.user.remove();
+		await req.user.remove();
+		// sendCancelationEmail(req.user.email, req.user.name);
 		res.send(req.user);
 	} catch (e) {
 		console.log(e);
