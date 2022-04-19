c4be41eb826a3a91caa97c139e81ae2f1a461820
Tylyps <jakubb.chmielewskii@gmail.com>
2022-04-19 22:22:34 +0200

    End of lessons 156-160 - events, sharing location, event acknowledgements and styling app

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,12 +1,70 @@
-const btn = document.querySelector("#increment");
+// const btn = document.querySelector("#increment");
+const chatContainer = document.querySelector("#chatContainer");
 const socket = io();
+const form = document.querySelector("#message-form");
+const input = document.querySelector("input");
+const formBtn = form.querySelector("button");
+const locationBtn = document.querySelector("#send-location");
 
-socket.on("countUpdated", (count) => {
-	console.log("The count has been updated! ", count);
-});
+// socket.on("countUpdated", (count) => {
+// 	console.log("The count has been updated! ", count);
+// });
 
-btn.addEventListener("click", () => {
-	console.log("Clicked")
-	socket.emit("Increment")
+// btn.addEventListener("click", () => {
+// 	console.log("Clicked")
+// 	socket.emit("Increment")
+// })
+
+socket.on("message", (message) => {
+	console.log(message)
+	const h = document.createElement("h4");
+	h.textContent = message;
+	chatContainer.appendChild(h);
+	chatContainer.scrollTo(0, chatContainer.scrollHeight)
 })
 
+
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const message = input.value.trim();
+	if (!message) {
+		return
+	}
+
+	formBtn.setAttribute("disabled", "disabled");
+
+	socket.emit("sendMessage", message, (error) => {
+		formBtn.removeAttribute("disabled");
+		if (error) {
+			return console.log(error);
+		}
+		console.log(message)
+	});
+	input.value = "";
+	input.focus();
+})
+
+locationBtn.addEventListener("click", (e) => {
+	if (!navigator.geolocation) {
+		return alert("Geolocation is not supported by your browser");
+	}
+
+	locationBtn.setAttribute("disabled", "disabled");
+	navigator.geolocation.getCurrentPosition((position) => {
+		console.log(position)
+		const location = {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		}
+		socket.emit("sendLocation", location, (error) => {
+			locationBtn.removeAttribute("disabled");
+			if (error) {
+				return console.log(error);
+			}
+
+			console.log("Location shared!")
+		});
+	})
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,16 +15,36 @@ const publicPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicPath))
 
-let count = 0;
-
 io.on("connection", (socket) => {
 	console.log("New WebSocket connection")
 
-	socket.emit("countUpdated", count)
+	socket.broadcast.emit("message", "A new user has joined")
 
-	socket.on("Increment", () => {
-		count++;
-		io.emit("countUpdated", count);
+	socket.emit("message", "Welcome!")
+
+	socket.on("sendMessage", (message, callback) => {
+		const filter = new Filter();
+
+		if (filter.isProfane(message)) {
+			return callback("Profanity is not allowed!");
+		}
+
+		io.emit("message", message);
+		if (callback) {
+			callback();
+		}
+	})
+
+	socket.on("sendLocation", (location, callback) => {
+		io.emit("message", `https://google.com/maps?q=${location.latitude},${location.longitude}`)
+
+		if (callback) {
+			callback()
+		}
+	})
+
+	socket.on("disconnect", () => {
+		io.emit("message", "A user has left!");
 	})
 })
 
